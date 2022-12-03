9ddb3ff1478bf820488d26783836584800fde773
Robert Chrzanowski <rchrzanowski@wayfair.com>
2022-12-03 00:26:33 -0500

    Day 3

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,15 +1,33 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun score(char: Char): Long {
+        return if (char.isLowerCase()) {
+            char - 'a' + 1L
+        } else {
+            char - 'A' + 27L
+        }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<String>): Long {
+        return input.sumOf { pack ->
+            pack.slice(0 until pack.length / 2).toSet()
+                .intersect(pack.slice(pack.length / 2 until pack.length).toSet())
+                .sumOf { score(it) }
+        }
+    }
+
+    fun part2(input: List<String>): Long {
+        return input.windowed(3, 3)
+            .sumOf { pack ->
+                pack.map { it.toSet() }
+                    .reduce { a, b -> a.intersect(b) }
+                    .sumOf { score(it) }
+            }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day03_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 157L)
+    check(part2(testInput) == 70L)
 
     val input = readInput("Day03")
     println(part1(input))
