685c0339c78f0d31e45c9c031c4729632f413499
Robert Chrzanowski <rchrzanowski@wayfair.com>
2022-12-08 10:00:42 -0500

    Day 8

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,18 +1,166 @@
+import java.lang.Exception
+import java.util.Stack
+
+//typealias Foo = Map<String, Foo>
+sealed interface Filesystem {
+    data class Directory(val nodes: MutableMap<String, Filesystem>) : Filesystem
+    data class File(val size: Long) : Filesystem
+}
+
+sealed interface Size {
+    data class Directory(override val size: Long, val nodes: Map<String, Size>) : Size
+    data class File(override val size: Long) : Size
+
+    val size: Long
+}
+
 fun main() {
     fun part1(input: List<String>): Long {
-        return input.size.toLong()
+        val groups = input.joinToString("\n").split(Regex("""\$ """))
+            .map { it.split("\n").dropLast(1) }
+            .drop(1)
+
+        val root = Filesystem.Directory(mutableMapOf())
+        val path = Stack<Filesystem.Directory>()
+        path.push(root)
+
+        groups.forEach { group ->
+            val (cmd) = group
+            val cdMatches = Regex("""^cd (.+)$""").matchEntire(cmd)
+            val lsMatches = Regex("""^ls$""").matchEntire(cmd)
+            if (cdMatches != null) {
+                val (dir) = cdMatches.destructured
+                if (dir == "/") {
+                    while (path.size > 1) {
+                        path.pop()
+                    }
+                } else if (dir == "..") {
+                    path.pop()
+                } else if (path.peek().nodes.containsKey(dir)) {
+                    path.push(path.peek().nodes[dir] as Filesystem.Directory)
+                } else {
+                    val directory = Filesystem.Directory(mutableMapOf())
+                    path.peek().nodes[dir] = directory
+                    path.push(directory)
+                }
+            } else if (lsMatches != null) {
+                val output = group.subList(1, group.size)
+                output.forEach { line ->
+                    val dirRegex = Regex("""^dir (.+)$""").matchEntire(line)
+                    val fileRegex = Regex("""^(\d+) (.+)$""").matchEntire(line)
+                    if (dirRegex != null) {
+                        val (directoryName) = dirRegex.destructured
+                        if (path.peek().nodes.containsKey(directoryName).not()) {
+                            path.peek().nodes[directoryName] = Filesystem.Directory(mutableMapOf())
+                        }
+                    } else if (fileRegex != null) {
+                        val (size, filename) = fileRegex.destructured
+                        path.peek().nodes[filename] = Filesystem.File(size.toLong())
+                    }
+                }
+            } else {
+                throw Exception()
+            }
+        }
+
+        fun directorySizes(filesystem: Filesystem): Pair<Long, List<Pair<Long, Filesystem>>> {
+            return when (filesystem) {
+                is Filesystem.File -> {
+                    Pair(filesystem.size, listOf(Pair(filesystem.size, filesystem)))
+                }
+
+                is Filesystem.Directory -> {
+                    val foo = filesystem.nodes.values.map { directorySizes(it) }
+                    val size = foo.sumOf { it.first }
+                    Pair(size, listOf(Pair(size, filesystem)).plus(foo.flatMap { it.second }))
+                }
+            }
+        }
+
+        return directorySizes(root).second
+            .filter { (size, filesystem) -> filesystem is Filesystem.Directory && size <= 100000L }
+            .sumOf { (size) -> size }
     }
 
     fun part2(input: List<String>): Long {
-        return input.size.toLong()
+        val groups = input.joinToString("\n").split(Regex("""\$ """))
+            .map { it.split("\n").takeWhile { it.isNotEmpty() } }
+            .drop(1)
+
+        val root = Filesystem.Directory(mutableMapOf())
+        val path = Stack<Filesystem.Directory>()
+        path.push(root)
+
+        groups.forEach { group ->
+            val (cmd) = group
+            val cdMatches = Regex("""^cd (.+)$""").matchEntire(cmd)
+            val lsMatches = Regex("""^ls$""").matchEntire(cmd)
+            if (cdMatches != null) {
+                val (dir) = cdMatches.destructured
+                if (dir == "/") {
+                    while (path.size > 1) {
+                        path.pop()
+                    }
+                } else if (dir == "..") {
+                    path.pop()
+                } else if (path.peek().nodes.containsKey(dir)) {
+                    path.push(path.peek().nodes[dir] as Filesystem.Directory)
+                } else {
+                    val directory = Filesystem.Directory(mutableMapOf())
+                    path.peek().nodes[dir] = directory
+                    path.push(directory)
+                }
+            } else if (lsMatches != null) {
+                val output = group.subList(1, group.size)
+                output.forEach { line ->
+                    val dirRegex = Regex("""^dir (.+)$""").matchEntire(line)
+                    val fileRegex = Regex("""^(\d+) (.+)$""").matchEntire(line)
+                    if (dirRegex != null) {
+                        val (directoryName) = dirRegex.destructured
+                        if (path.peek().nodes.containsKey(directoryName).not()) {
+                            path.peek().nodes[directoryName] = Filesystem.Directory(mutableMapOf())
+                        }
+                    } else if (fileRegex != null) {
+                        val (size, filename) = fileRegex.destructured
+                        path.peek().nodes[filename] = Filesystem.File(size.toLong())
+                    }
+                }
+            } else {
+                throw Exception()
+            }
+        }
+
+        fun directorySizes(filesystem: Filesystem): Size {
+            return when (filesystem) {
+                is Filesystem.File -> {
+                    Size.File(filesystem.size)
+                }
+
+                is Filesystem.Directory -> {
+                    val nodes = filesystem.nodes.mapValues { (_, it) -> directorySizes(it) }
+                    Size.Directory(nodes.values.sumOf { it.size }, nodes)
+                }
+            }
+        }
+
+        val rootSize = directorySizes(root) as Size.Directory
+
+        println(rootSize)
+
+        fun flattenSize(size: Size.Directory): List<Size> {
+            return listOf(size).plus(size.nodes.values.filterIsInstance<Size.Directory>().flatMap { flattenSize(it) })
+        }
+
+        return flattenSize(rootSize).sortedBy { it.size }
+            .first { (70000000 - rootSize.size) + it.size >= 30000000 }.size
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day07_test")
-    check(part1(testInput) == 1L)
-    check(part2(testInput) == 1L)
-
+    check(part1(testInput) == 95437L)
     val input = readInput("Day07")
     println(part1(input))
+
+    check(part2(testInput) == 24933642L)
     println(part2(input))
 }
--- src/Day08.kt
+++ src/Day08.kt
@@ -1,18 +1,125 @@
 fun main() {
-    fun part1(input: List<String>): Long {
-        return input.size.toLong()
+    fun part1(input: List<String>): Int {
+        val trees = input.map { row -> row.map { it - '0' } }
+        val visible = trees.map { it.indices.map { false }.toMutableList() }
+
+        trees.first().indices.forEach { c ->
+            fun searchNorth(r: Int, h: Int) {
+                if (r >= trees.size) {
+                    return
+                } else if (trees[r][c] > h) {
+                    visible[r][c] = true
+                    searchNorth(r + 1, trees[r][c])
+                } else {
+                    searchNorth(r + 1, h)
+                }
+            }
+
+            searchNorth(0, -1)
+        }
+
+        trees.first().indices.forEach { c ->
+            fun searchSouth(r: Int, h: Int) {
+                if (r < 0) {
+                    return
+                } else if (trees[r][c] > h) {
+                    visible[r][c] = true
+                    searchSouth(r - 1, trees[r][c])
+                } else {
+                    searchSouth(r - 1, h)
+                }
+            }
+
+            searchSouth(trees.size - 1, -1)
+        }
+
+        trees.indices.forEach { r ->
+            fun searchWest(c: Int, h: Int) {
+                if (c >= trees.size) {
+                    return
+                } else if (trees[r][c] > h) {
+                    visible[r][c] = true
+                    searchWest(c + 1, trees[r][c])
+                } else {
+                    searchWest(c + 1, h)
+                }
+            }
+
+            searchWest(0, -1)
+        }
+
+        trees.indices.forEach { r ->
+            fun searchEast(c: Int, h: Int) {
+                if (c < 0) {
+                    return
+                } else if (trees[r][c] > h) {
+                    visible[r][c] = true
+                    searchEast(c - 1, trees[r][c])
+                } else {
+                    searchEast(c - 1, h)
+                }
+            }
+
+            searchEast(trees.size - 1, -1)
+        }
+
+        return visible.sumOf { it.count { it } }
     }
 
-    fun part2(input: List<String>): Long {
-        return input.size.toLong()
+    fun part2(input: List<String>): Int {
+        val trees = input.map { row -> row.map { it - '0' } }
+
+        val scenicScore = trees.indices.map { r ->
+            trees.first().indices.map { c ->
+                val max = trees[r][c]
+
+                tailrec fun searchNorth(r: Int, acc: Int): Int {
+                    return when {
+                        r < 0 -> acc
+                        trees[r][c] >= max -> acc + 1
+                        else -> searchNorth(r - 1, acc + 1)
+                    }
+                }
+
+                tailrec fun searchSouth(r: Int, acc: Int): Int {
+                    return when {
+                        r >= trees.size -> acc
+                        trees[r][c] >= max -> acc + 1
+                        else -> searchSouth(r + 1, acc + 1)
+                    }
+                }
+
+                tailrec fun searchEast(c: Int, acc: Int): Int {
+                    return when {
+                        c >= trees.size -> acc
+                        trees[r][c] >= max -> acc + 1
+                        else -> searchEast(c + 1, acc + 1)
+                    }
+                }
+
+                tailrec fun searchWest(c: Int, acc: Int): Int {
+                    return when {
+                        c < 0 -> acc
+                        trees[r][c] >= max -> acc + 1
+                        else -> searchWest(c - 1, acc + 1)
+                    }
+                }
+
+                searchNorth(r - 1, 0) *
+                        searchSouth(r + 1, 0) *
+                        searchEast(c + 1, 0) *
+                        searchWest(c - 1, 0)
+            }
+        }
+
+        return scenicScore.maxOf { row -> row.maxOf { it } }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day08_test")
-    check(part1(testInput) == 1L)
-    check(part2(testInput) == 1L)
-
+    check(part1(testInput) == 21)
     val input = readInput("Day08")
     println(part1(input))
+    check(part2(testInput) == 8)
     println(part2(input))
 }
